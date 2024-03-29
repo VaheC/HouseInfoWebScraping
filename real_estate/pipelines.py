f7d4e44e206f96ea8fef7b1986a9729e949935af
VaheC <vcharchian@gmail.com>
2024-03-29 16:32:44 +0400

    initial pipeline added

--- real_estate/pipelines.py
+++ real_estate/pipelines.py
@@ -6,8 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from datetime import datetime as dt
 
 
 class RealEstatePipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        value = adapter.get('price_usd')
+        adapter['price_usd'] = int(value.replace('$', '').replace(',', '').strip())
+
+        value = adapter.get('price_amd')
+        adapter['price_amd'] = int(value.replace('֏', '').replace(',', '').strip())
+
+        value = adapter.get('price_rub')
+        adapter['price_rub'] = int(value.replace('₽', '').replace(',', '').strip())
+
+        for field_name in ['address', 'placesby_text', 'seller_id', 'seller']:
+            value = adapter.get(field_name)
+            adapter[field_name] = value.strip()
+
+        value = adapter.get('description_text')
+        adapter['description_text'] = value.replace('<br>', '\n').strip()
+
+        for field_name in ['posted_date', 'renewed_date']:
+            temp_replace_value = field_name.split('_')[0].capitalize()
+            value = adapter.get(field_name)
+            adapter[field_name] = dt.strptime(
+                '-'.join(value.replace(f'{temp_replace_value} ', '').strip().split(' ')[0].split('.')[::-1]), 
+                '%Y-%m-%d'
+            )
+
         return item
